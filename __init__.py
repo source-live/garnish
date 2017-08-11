7be2bfc6cb6758d2086881397e1501dfd42dd6e1
Zack P <zack@zacklearns.com>
2017-08-11 10:28:41 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
-class test:
-  def __init__()
-    print("Hello World")
+import os
+
+class website:
+  wpath_ = ""
+  def __init__(name,path):
+    # Name is a string, path is an array: website(string name, array path)
+    p = "/".join(path)
+    pc = "".join(["C:/",p])
+    os.mkdirs(pc)
+    print("".join(["Successfully made website @ ",pc]))
+    if name and path == None:
+      return False
+    else:
+      return True
