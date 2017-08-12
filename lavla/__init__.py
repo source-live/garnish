eb62c54e10d2c01f425cc97c088a50c2d188fafa
Zack P <zack@zacklearns.com>
2017-08-12 11:43:33 -0600

    Update __init__.py

--- lavla/__init__.py
+++ lavla/__init__.py
@@ -21,9 +21,23 @@ class lavla:
   _page_alias = c_PAGE_ALIAS
   _vers = __version__
   
+  def __init__(_n,_pn,_pa)
+    c_WEB_NAME = _n
+    _web_name = c_WEB_NAME
+    
+    c_PAGE_NAME = _pn
+    _page_name = c_PAGE_NAME
+    
+    c_PAGE_ALIAS = _pa
+    _page_alias = c_PAGE_ALIAS
   
-  def __init__(name,path,pre):
+  def new(name,path,pre):
     name = "".join([name,ext])
+    
+    for n = pre:
+      n = n.format(nm=_webname)
+    
+    path = "/".join(path)
     path = os.path.join(path,name)
     if not os.path.exists(path):
       os.makedirs(path)
